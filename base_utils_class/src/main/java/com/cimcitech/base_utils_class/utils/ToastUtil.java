fd1823d2898de9ebdea0210d1c046c0737e6e6ca
ken <xiongkang@cimc.com>
2019-08-28 12:45:35 +0800

    优化基类的toast 工具（定制贝斯特的需求）

--- base_utils_class/src/main/java/com/cimcitech/base_utils_class/utils/ToastUtil.java
+++ base_utils_class/src/main/java/com/cimcitech/base_utils_class/utils/ToastUtil.java
@@ -4,16 +4,26 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.cimcitech.base_utils_class.base.BaseLibrary;
+
+import es.dmoral.toasty.Toasty;
+
 public class ToastUtil {
     private static Toast toast;
 
     @SuppressLint("ShowToast")
     public static void showToast(Context context, String content) {
-        if (toast == null) {
-            toast = Toast.makeText(context, content, Toast.LENGTH_SHORT);
+
+        if ("com.cimctech.bestscancode_android".equals(BaseLibrary.packageName) ||
+                "com.cimcitech.best_waybill".equals(BaseLibrary.packageName)) {
+            Toasty.error(context, content, Toast.LENGTH_SHORT, true).show();
         } else {
-            toast.setText(content);
+            if (toast == null) {
+                toast = Toast.makeText(context, content, Toast.LENGTH_SHORT);
+            } else {
+                toast.setText(content);
+            }
+            toast.show();
         }
-        toast.show();
     }
 }
