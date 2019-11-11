a1de9ab7e05410419b1e48506c785d40f21ede0d
ken <373580308@qq.com>
2019-11-11 10:37:15 +0800

    移除sdk api

--- base_api_sdk/src/main/java/com/cimcitech/base_api_sdk/base/OnBaseCallback.java
+++
@@ -1,5 +0,0 @@
-package com.cimcitech.base_api_sdk.base;
-
-public interface OnBaseCallback {
-    void onError(String code, String error);
-}
--- base_api_sdk/src/main/java/com/cimcitech/base_api_sdk/base/ResultCode.java
+++
@@ -1,8 +0,0 @@
-package com.cimcitech.base_api_sdk.base;
-
-public class ResultCode {
-    public static final String REQUEST_SUCCESS = "00";
-    public static final String error_network_anomaly = "54";
-    public static final String request_failure = "400";
-    public static final String error_not_json = "202";//JSON 数据有误
-}
